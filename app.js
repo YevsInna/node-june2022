68e43ef21c47750a685f654f0f6af0f82c3e244b
YevsInna <evseinka@gmail.com>
2022-11-13 11:00:53 +0100

    lesson 1 fs hw path.join

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const fs = require('fs/promises');
+
+const path = require('path');
+
 //
 // fs.mkdir('./female', (err)=>{
 //     console.log(err)
@@ -27,19 +30,44 @@ const fs = require('fs');
 // })
 
 
-fs.readdir('./male', (err5, files) => {
-    for (const file of files) {
-        fs.stat(`./male/${file}`, (err6, stats) => {
-            if (stats.isFile()) {
-                fs.readFile(`./male/${file}`, (err7, data2) => {
-                    const user = JSON.parse(data2)
-                    if (user.gender === 'female') {
-                        fs.rename(`./male/${file}`, `./female/${file}`, (err8) => {
-                            console.log(err8)
-                        })
-                    }
-                })
+// fs.readdir('./male', (err5, files) => {
+//     for (const file of files) {
+//         fs.stat(`./male/${file}`, (err6, stats) => {
+//             if (stats.isFile()) {
+//                 fs.readFile(`./male/${file}`, (err7, data2) => {
+//                     const user = JSON.parse(data2)
+//                     if (user.gender === 'female') {
+//                         fs.rename(`./male/${file}`, `./female/${file}`, (err8) => {
+//                             console.log(err8)
+//                         })
+//                     }
+//                 })
+//             }
+//         })
+//     }
+// })
+
+const sorter = async (readFolder, writeFolder, gender) => {
+
+    try {
+        const folderPath = path.join(__dirname, readFolder);
+
+        const files = await fs.readdir(folderPath);
+
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const data = await fs.readFile(filePath);
+            const user = JSON.parse(data);
+
+            if (user.gender === gender){
+                await fs.rename(filePath, path.join(__dirname, writeFolder, file));
             }
-        })
+        }
+    }catch (e){
+        console.error(e)
     }
-})
+
+}
+
+sorter('female','male', 'male');
+sorter('male','female', 'female');
