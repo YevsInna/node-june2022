e36c5ae7e344ea990f223ae3dab82501d5b7fae6
YevsInna <evseinka@gmail.com>
2022-12-05 14:20:07 +0100

    hw-6 email

--- config/config.js
+++ config/config.js
@@ -3,4 +3,7 @@ module.exports = {
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test-project',
     SECRET_ACCESS: process.env.SECRET_ACCESS || 'secret access word',
     SECRET_REFRESH: process.env.SECRET_REFRESH || 'secret refresh word',
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL,
+    NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD
+
 }
